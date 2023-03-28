2a5960c4b04893d9c84530f6a3e32a75a154f7c2
Ankit <ankitsharmagh093@gmail.com>
2023-03-29 02:27:18 +0530

    second page created

--- components/Form/Second.js
+++ components/Form/Second.js
@@ -2,11 +2,36 @@ import styles from '@/styles/Second.module.scss';
 import BtnGroup from '../utils/BtnGroup';
 import Container from '../utils/Container';
 import Page from '../utils/Page';
+import country from '@/public/countryNames.json';
+import { InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import LanguagePicker from './Second/LanguagePicker';
+const arr = Object.entries(country);
 
 const Second = () => {
   return (
     <Container>
-      <Page></Page>
+      <Page>
+        <InputLabel id="demo-simple-select-label">Country of origin</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Country of origin"
+          style={{ background: '#E2F2FF' }}
+        >
+          {arr.map((item, count) => (
+            <MenuItem value={item[1].country} key={count}>
+              {item[1].country}
+            </MenuItem>
+          ))}
+        </Select>
+        <TextField
+          id="filled-basic"
+          label="Phone Number"
+          variant="outlined"
+          style={{ width: '100%', background: '#E2F2FF' }}
+        />
+        <LanguagePicker />
+      </Page>
       <BtnGroup prev="/form/first" next={'/form/third'} />
     </Container>
   );
---
+++ components/Form/Second/LanguagePicker.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import language from '@/public/languageNames.json';
+import styles from '@/styles/LanguagePicker.module.scss';
+import DeleteIcon from '@mui/icons-material/Delete';
+const {
+  Button,
+  InputLabel,
+  Select,
+  MenuItem,
+  Rating,
+  IconButton,
+} = require('@mui/material');
+const languageArr = Object.entries(language);
+
+const LanguagePicker = () => {
+  const [counter, setCount] = useState(0);
+  const [arr, setArray] = useState([]);
+  useEffect(() => {
+    const array = new Array(counter).fill(0);
+    setArray(array);
+  }, [counter]);
+
+  const buttonHandler = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  //   languageArr.forEach((item) => console.log(item[1].name));
+  return (
+    <div>
+      <Button onClick={buttonHandler}>Add Language</Button>
+      {arr.map((item, count) => (
+        <div key={count} className={styles.languageContainer}>
+          <div className={styles.language}>
+            <InputLabel id="language-select">Language</InputLabel>
+            <Select
+              fullWidth
+              labelId="language-select"
+              id="language-select"
+              label="Select Language"
+              style={{ background: '#E2F2FF' }}
+            >
+              {languageArr.map((value, count) => (
+                <MenuItem value={value[1].name} key={count}>
+                  {value[1].name}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
+          <div>
+            <InputLabel id="rating-select">Fluency</InputLabel>
+            <Rating
+              name="simple-controlled"
+              size="large"
+              labelId="rating-select"
+            />
+          </div>
+          <IconButton
+            className={styles.delete}
+            size="large"
+            color="primary"
+            onClick={() => {
+              console.log('I am delete');
+              setCount((prev) => prev - 1);
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LanguagePicker;
